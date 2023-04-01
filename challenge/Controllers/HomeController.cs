85c1867dc29942dd4e3178763c4320ff1b20baa1
Joe Lambert <jmlambert58@gmail.com>
2023-03-31 19:40:07 -0600

    Add project files.

---
+++ challenge/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using challenge.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace challenge.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult Apply()
+        {
+            return View();
+        }
+
+    }
+}
